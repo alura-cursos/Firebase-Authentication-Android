1cd1a24912fc42bc2840cf60dd816baede7d45d2
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-18 11:52:25 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/CadastroUsuarioFragment.kt
@@ -0,0 +1,42 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.ComponentesVisuais
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
+import kotlinx.android.synthetic.main.cadastro_usuario.*
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+
+class CadastroUsuarioFragment : Fragment() {
+
+    private val controlador by lazy {
+        findNavController()
+    }
+    private val estadoAppViewModel: EstadoAppViewModel by sharedViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.cadastro_usuario,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        estadoAppViewModel.temComponentes = ComponentesVisuais()
+        cadastro_usuario_botao_cadastrar.setOnClickListener {
+            controlador.popBackStack()
+        }
+    }
+
+}
