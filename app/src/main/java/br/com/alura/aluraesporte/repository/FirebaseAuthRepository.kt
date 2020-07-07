c043893b77498d79fdef700ba811000f375f990f
Alex Felipe <alexfelipevieira@gmail.com>
2020-07-07 13:50:14 -0300

    4.1

--- app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
+++ app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
@@ -3,7 +3,9 @@ package br.com.alura.aluraesporte.repository
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import br.com.alura.aluraesporte.model.Usuario
 import com.google.firebase.auth.*
+import java.lang.Exception
 import java.lang.IllegalArgumentException
 
 private const val TAG = "FirebaseAuthRepository"
@@ -29,29 +31,31 @@ class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
             }
     }
 
-    fun cadastra(email: String, senha: String): LiveData<Resource<Boolean>> {
+    fun cadastra(usuario: Usuario): LiveData<Resource<Boolean>> {
         val liveData = MutableLiveData<Resource<Boolean>>()
         try {
             val tarefa =
-                firebaseAuth.createUserWithEmailAndPassword(email, senha)
+                firebaseAuth.createUserWithEmailAndPassword(usuario.email, usuario.senha)
             tarefa.addOnSuccessListener {
                 Log.i(TAG, "cadastra: cadastro sucedido")
                 liveData.value = Resource(true)
             }
             tarefa.addOnFailureListener {exception ->
                 Log.e(TAG, "cadastra: cadastro falhou", exception)
-                val mensagemErro: String = when(exception){
-                    is FirebaseAuthWeakPasswordException -> "Senha precisa de pelo menos 6 dígitos"
-                    is FirebaseAuthInvalidCredentialsException -> "E-mail inválido"
-                    is FirebaseAuthUserCollisionException -> "E-mail já cadastrado"
-                    else -> "Erro desconhecido"
-                }
+                val mensagemErro: String = devolveErroDeCadastro(exception)
                 liveData.value = Resource(false, mensagemErro)
             }
         } catch (e: IllegalArgumentException) {
             liveData.value = Resource(false, "E-mail ou senha não ser vazio")
         }
         return liveData
+    }
+
+    private fun devolveErroDeCadastro(exception: Exception): String = when (exception) {
+        is FirebaseAuthWeakPasswordException -> "Senha precisa de pelo menos 6 dígitos"
+        is FirebaseAuthInvalidCredentialsException -> "E-mail inválido"
+        is FirebaseAuthUserCollisionException -> "E-mail já cadastrado"
+        else -> "Erro desconhecido"
     }
 
 }
--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/CadastroUsuarioFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/CadastroUsuarioFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.extensions.snackBar
+import br.com.alura.aluraesporte.model.Usuario
 import br.com.alura.aluraesporte.ui.viewmodel.CadastroUsuarioViewModel
 import br.com.alura.aluraesporte.ui.viewmodel.ComponentesVisuais
 import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
@@ -39,56 +41,68 @@ class CadastroUsuarioFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         estadoAppViewModel.temComponentes = ComponentesVisuais()
+        configuraBotaoCadastro()
+    }
+
+    private fun configuraBotaoCadastro() {
         cadastro_usuario_botao_cadastrar.setOnClickListener {
 
-            cadastro_usuario_email.error = null
-            cadastro_usuario_senha.error = null
-            cadastro_usuario_confirma_senha.error = null
+            limpaTodosCampos()
 
             val email = cadastro_usuario_email.editText?.text.toString()
             val senha = cadastro_usuario_senha.editText?.text.toString()
             val confirmaSenha = cadastro_usuario_confirma_senha.editText?.text.toString()
 
-            var valido = true
+            val valido = validaCampos(email, senha, confirmaSenha)
 
-            if(email.isBlank()){
-                cadastro_usuario_email.error = "E-mail é necessário"
-                valido = false
-            }
-
-            if(senha.isBlank()){
-                cadastro_usuario_senha.error = "Senha é necessária"
-                valido = false
-            }
-
-            if(senha != confirmaSenha){
-                cadastro_usuario_confirma_senha.error = "Senhas diferentes"
-                valido = false
-            }
-
-            if(valido){
-                viewModel.cadastra(email, senha).observe(viewLifecycleOwner, Observer {
-                    it?.let {recurso ->
-                        if(recurso.dado){
-                            Snackbar.make(
-                                view,
-                                "Cadastro realizado com sucesso",
-                                Snackbar.LENGTH_SHORT
-                            ).show()
-                            controlador.popBackStack()
-                        } else {
-                            val mensagemErro = recurso.erro ?: "Ocorreu uma falha no cadastro"
-                            Snackbar.make(
-                                view,
-                                mensagemErro,
-                                Snackbar.LENGTH_SHORT
-                            ).show()
-                        }
-
-                    }
-                })
+            if (valido) {
+                cadastra(Usuario(email, senha))
             }
         }
+    }
+
+    private fun cadastra(usuario: Usuario) {
+        viewModel.cadastra(usuario).observe(viewLifecycleOwner, Observer {
+            it?.let { recurso ->
+                if (recurso.dado) {
+                    view?.snackBar("Cadastro realizado com sucesso")
+                    controlador.popBackStack()
+                } else {
+                    val mensagemErro = recurso.erro ?: "Ocorreu uma falha no cadastro"
+                    view?.snackBar(mensagemErro)
+                }
+            }
+        })
+    }
+
+    private fun validaCampos(
+        email: String,
+        senha: String,
+        confirmaSenha: String
+    ): Boolean {
+        var valido = true
+
+        if (email.isBlank()) {
+            cadastro_usuario_email.error = "E-mail é necessário"
+            valido = false
+        }
+
+        if (senha.isBlank()) {
+            cadastro_usuario_senha.error = "Senha é necessária"
+            valido = false
+        }
+
+        if (senha != confirmaSenha) {
+            cadastro_usuario_confirma_senha.error = "Senhas diferentes"
+            valido = false
+        }
+        return valido
+    }
+
+    private fun limpaTodosCampos() {
+        cadastro_usuario_email.error = null
+        cadastro_usuario_senha.error = null
+        cadastro_usuario_confirma_senha.error = null
     }
 
 }
